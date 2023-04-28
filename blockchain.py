77232c78b0e1be1ec123924e5fbaa73821716ac9
Jorge Sader <Jorge.Sader@gmail.com>
2023-04-28 17:23:37 -0400

    refactor to add conditionals on functions

--- blockchain.py
+++ blockchain.py
@@ -2,10 +2,12 @@ blockchain = [[1]]
 
 def get_last_blockchain_value():
   ''' gets and returns the last value of the blockchain.'''
+  if len(blockchain) < 1:
+    return None 
   return blockchain[-1]
 
 
-def add_value(transaction_amount, last_transaction=[1]):
+def add_transaction(transaction_amount, last_transaction=[1]):
   '''it appends a given transaction amount and last transacion to the current blockchain.
   
   Arguments:
@@ -13,6 +15,8 @@ def add_value(transaction_amount, last_transaction=[1]):
     last_transaction: the transaction that was made prior to the transaction being made.
   
   '''
+  if last_transaction == None:
+    last_transaction = [1]
   blockchain.append([last_transaction, transaction_amount])
 
 
@@ -31,18 +35,15 @@ def print_blockchain_elements():
     print(block)
     
     
-add_value(get_transaction_value())
 
 while True:
   print('Please Choose: ')
   print('1: Add a new transaction value.')
   print('2: Output the blockchain blocks.')
   print("x: Exit.")
-  
   user_choice = get_user_choice()
-  
   if user_choice == '1':
-    add_value(get_transaction_value(), get_last_blockchain_value())
+    add_transaction(get_transaction_value(), get_last_blockchain_value())
   elif user_choice=='2':
     print_blockchain_elements()
   elif user_choice == 'x':
@@ -50,9 +51,4 @@ while True:
   else:
     print('Sorry please enter a valid option.')
     
-  
-  
-  
-  
-  
 print('Done!!!')

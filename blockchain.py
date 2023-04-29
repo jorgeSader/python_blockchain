1afa7a1ea67f7589152e2090f7fe8dd727db887b
Jorge Sader <Jorge.Sader@gmail.com>
2023-04-29 10:42:28 -0400

    refactor loop exit and add visual lines for UX

--- blockchain.py
+++ blockchain.py
@@ -34,6 +34,8 @@ def print_blockchain_elements():
     for block in blockchain:
         print('Outputting Block: ')
         print(block)
+    else:
+        print('-' * 35)
 
 
 def verify_chain():
@@ -52,19 +54,23 @@ def verify_chain():
     return is_valid
 
 
-while True:
+waiting_for_input = True
+while waiting_for_input:
+    print('-' * 35)
     print('Please Choose: ')
     print('1: Add a new transaction value.')
     print('2: Output the blockchain blocks.')
     print("h: Manipulate the chain (hack!).")
-    print("x: Exit.")
+    print("q: Exit.")
+    print('-' * 35)
+
     user_choice = get_user_choice()
     if user_choice == '1':
         add_transaction(get_transaction_value(), get_last_blockchain_value())
     elif user_choice == '2':
         print_blockchain_elements()
-    elif user_choice == 'x':
-        break
+    elif user_choice == 'q':
+        waiting_for_input = False
     elif user_choice == 'h':
         if len(blockchain) >= 1:
             blockchain[0] = 2
@@ -73,6 +79,7 @@ while True:
 
     if not verify_chain():
         print('Invalid Blockchain!')
+        print('You Have been logged out for tampering!!!')
         break
 
-print('Done!!!')
+print('Goodbye ;)')
